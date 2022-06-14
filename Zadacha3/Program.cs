8e8b1882b01b86284be3d09fa54f0490f7bf969d
Anastasiu <anastastasiu.9315@gmail.com>
2022-06-14 16:16:33 +1100

    z3=

--- Zadacha3/Program.cs
+++ Zadacha3/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+
+int WeekDays = int.Parse(Console.ReadLine());
+
+if (WeekDays == 1)
+{
+    Console.WriteLine("Monday");
+}
+else if (WeekDays == 2)
+{
+    Console.WriteLine("Tuesday");
+}
+else if (WeekDays == 3)
+{
+    Console.WriteLine("Wednesday");
+    }
+else if (WeekDays == 4)
+{
+    Console.WriteLine("Thursday");
+}
+else if (WeekDays == 5)
+{
+    Console.WriteLine("Friday");
+}
+else if (WeekDays == 6)
+{
+    Console.WriteLine("Saturday");
+}
+else if (WeekDays == 7)
+{
+    Console.WriteLine("Sunday ");
+}
+else
+{
+  Console.WriteLine("error");  
+}
